53194eb46abfa19a251e4bc72ced4f81874409c2
Sandeep Yadav <sandeep@bookmyinsurance.com>
2025-10-27 17:22:54 +0530

    --inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+export default config;
